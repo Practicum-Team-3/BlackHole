5aba75c56acd9aad166e806fbc5359d42f7054e8
Aaron Himan <amhiman@miners.utep.edu>
2020-03-02 13:29:47 -0700

    new folder because of file structure

--- Screens/DataHandler/BHMachine.py
+++ Screens/DataHandler/BHMachine.py
@@ -10,6 +10,25 @@ class BHMachine(object):
         self.network_settings = None #should be dictionary
         self.provisions = None #should be list
 
+        ### initialize Machine Settings window attributes
+        self.baseMemory = 4
+        self.bootOrder_floppy = False
+        self.bootOrder_optical = False
+        self.bootOrder_hardDisk = False
+        self.bootOrder_network = False
+        self.chipset = ''
+        self.pointingDevice = ''
+        self.extFeat_enableIO = False
+        self.extFeat_enableEFI = False
+        self.extFeat_hwClockUTCtime = False
+        self.processors = 1
+        self.exeCap = 100
+        self.extFeat_enablePAE_NX = False
+        self.paravirtInterface = ''
+        self.hdVirt_enableVTx = False
+        self.hdVirt_enableNestedPaging = False
+        ### end machine settings window attributes
+
         if machineJSON != '':
             self.fromJSON(machineJSON)
 
@@ -38,14 +57,130 @@ class BHMachine(object):
         self.id = jsonObject['id']
         self.os = jsonObject['os']
         self.type = jsonObject['type']
+
+        ### machine settings window fields
+        self.baseMemory = jsonObject['baseMemory']
+        self.bootOrder_hardDisk = jsonObject['bootOrder_hardDisk']
+        self.bootOrder_optical = jsonObject['bootOrder_optical']
+        self.bootOrder_floppy = jsonObject['bootOrder_floppy']
+        self.bootOrder_network = jsonObject['bootOrder_network']
+        self.chipset = jsonObject['chipset']
+        self.pointingDevice = jsonObject['pointingDevice']
+        self.extFeat_enableIO = jsonObject['extFeat_enableIO']
+        self.extFeat_enableEFI = jsonObject['extFeat_enableEFI']
+        self.extFeat_hwClockUTCtime = jsonObject['extFeat_hwClockUTCtime']
+        self.processors = jsonObject['processors']
+        self.exeCap = jsonObject['exeCap']
+        self.extFeat_enablePAE_NX = jsonObject['extFeat_EnablePAE_NX']
+        self.paravirtInterface = jsonObject['paravirtInterface']
+        self.hdVirt_enableVTx = jsonObject['hdVirt_enableVTx']
+        self.hdVirt_enableNestedPaging = jsonObject['hdVirt_enableNestedPaging']
         self.shared_folders = BHSharedFolders(jsonObject['shared_folders'])
         self.network_settings = BHNetworkSettings(jsonObject['network_settings'])
         self.provisions = BHProvisions(jsonObject['provisions'])
+        ### end machine settings window fields
 
     #returns a JSON string version of this object
     def toJSON(self):
         pass
 
+    ### getter and setter for machine settings
+    def getBaseMemory(self):
+        return self.baseMemory
+
+    def setBaseMemory(self, baseMemory):
+        self.baseMemory = baseMemory
+
+    def getBootOrder_hardDisk(self):
+        return self.bootOrder_hardDisk
+
+    def setBootOrder_hardDisk(self, bootOrder_hardDisk):
+        self.bootOrder_hardDisk = bootOrder_hardDisk
+
+    def getBootOrder_optical(self):
+        return self.bootOrder_optical
+
+    def setBootOrder_optical(self, bootOrder_optical):
+        self.bootOrder_optical = bootOrder_optical
+
+    def getBootOrder_floppy(self):
+        return self.bootOrder_floppy
+
+    def setBootOrder_floppy(self, bootOrder_floppy):
+        self.bootOrder_floppy = bootOrder_floppy
+
+    def getBootOrder_network(self):
+        return self.bootOrder_network
+
+    def setBootOrder_network(self, bootOrder_network):
+        self.bootOrder_network = bootOrder_network
+
+    def getChipset(self):
+        return self.chipset
+
+    def setChipset(self, chipset):
+        self.chipset = chipset
+
+    def getPointingDevice(self):
+        return self.pointingDevice
+
+    def setPointingDevice(self, pointingDevice):
+        self.pointingDevice = pointingDevice
+
+    def getExtFeat_enableIO(self):
+        return self.extFeat_enableIO
+
+    def setExtFeat_enableIO(self, extFeat_enableIO):
+        self.extFeat_enableIO = extFeat_enableIO
+
+    def getExtFeat_enableEFI_network(self):
+        return self.extFeat_enableEFI
+
+    def setExtFeat_enableEFI(self, extFeat_enableEFI):
+        self.extFeat_enableEFI = extFeat_enableEFI
+
+    def getExtFeat_hwClockUTCtime(self):
+        return self.extFeat_hwClockUTCtime
+
+    def setExtFeat_hwClockUTCtime(self, extFeat_hwClockUTCtime):
+        self.extFeat_hwClockUTCtime = extFeat_hwClockUTCtime
+
+    def getProcessors(self):
+        return self.processors
+
+    def setProcessors(self, processors):
+        self.processors = processors
+
+    def getExeCap(self):
+        return self.exeCap
+
+    def setExeCap(self, exeCap):
+        self.exeCap = exeCap
+
+    def getExtFeat_enablePAE_NX(self):
+        return self.extFeat_enablePAE_NX
+
+    def setExtFeat_enablePAE_NX(self, extFeat_enablePAE_NX):
+        self.extFeat_enablePAE_NX = extFeat_enablePAE_NX
+    
+    def getParavirtInterface(self):
+        return self.paravirtInterface
+
+    def setParavirtInterface(self, paravirtInterface):
+        self.paravirtInterface = paravirtInterface
+
+    def getHdVirt_enableVTx(self):
+        return self.hdVirt_enableVTx
+
+    def setHdVirt_enableVTx(self, hdVirt_enableVTx):
+        self.hdVirt_enableVTx = hdVirt_enableVTx
+
+    def getHdVirt_enableNestedPaging(self):
+        return self.hdVirt_enableNestedPaging
+
+    def setHdVirt_enableNestedPaging(self, hdVirt_enableNestedPaging):
+        self.hdVirt_enableNestedPaging = hdVirt_enableNestedPaging
+    ### end getter and setter for machine settings
 
 #HELPER CLASSES
 class BHSharedFolders(object):
@@ -70,3 +205,9 @@ class BHProvisions(object):
 
     def getDict(self):
         return self.dictionary
+
+
+
+
+
+    
---
+++ Screens/MachineSettings.py
@@ -0,0 +1,188 @@
+import sys
+import json
+from PySide2.QtWidgets import QApplication, QMainWindow, QDialog
+from PySide2.QtCore import QFile
+from Interfaces.ui_machineSettings import Ui_MachineSettings
+from BHMachine import BHMachine
+
+# will take a BHMachine instance
+class MachineSettings(QDialog):
+    def __init__(self, BHMachine):
+        super(MachineSettings, self).__init__()
+        self.machineObject = BHMachine
+        self.ui = Ui_MachineSettings()
+        self.ui.setupUi(self)
+
+        # set machine state
+        self.setState()
+
+        self.ui.OverlayButtonBox.accepted.connect(self.okPressed)
+        self.ui.OverlayButtonBox.rejected.connect(self.cancelPressed)
+    
+    # set the state of the machine when user first enters window
+    def setState(self):
+        self.ui.spinbox_baseMemory.setValue(self.machineObject.getBaseMemory())
+        
+        if (self.machineObject.getBootOrder_hardDisk() == "True"): # was in json as str
+            self.ui.bootOrderCheckbox_hardDisk.setChecked(True)
+        else:
+            self.ui.bootOrderCheckbox_hardDisk.setChecked(False)
+
+        if (self.machineObject.getBootOrder_optical() == "True"): # was in json as str
+            self.ui.bootOrderCheckbox_optical.setChecked(True)
+        else:
+            self.ui.bootOrderCheckbox_optical.setChecked(False)
+        
+        if (self.machineObject.getBootOrder_floppy() == "True"): # was in json as str
+            self.ui.bootOrderCheckBox_floppy.setChecked(True)
+        else:
+            self.ui.bootOrderCheckBox_floppy.setChecked(False)
+        
+        if (self.machineObject.getBootOrder_network() == "True"): # was in json as str
+            self.ui.bootOrderCheckbox_network.setChecked(True)
+        else:
+            self.ui.bootOrderCheckbox_network.setChecked(False)
+        
+
+        self.ui.comboBox_chipset.setCurrentText(self.machineObject.getChipset())
+        self.ui.comboBox_pointingDevice.setCurrentText(self.machineObject.getPointingDevice())
+
+        if (self.machineObject.getExtFeat_enableIO() == "True"): # was in json as str
+            self.ui.extFeatCheckbox_enableIO.setChecked(True)
+        else:
+            self.ui.extFeatCheckbox_enableIO.setChecked(False)
+        
+        if (self.machineObject.getExtFeat_enableEFI_network() == "True"): # was in json as str
+            self.ui.extFeatCheckBox_enableEFI.setChecked(True)
+        else:
+            self.ui.extFeatCheckBox_enableEFI.setChecked(False)
+
+        if (self.machineObject.getExtFeat_hwClockUTCtime() == "True"): # was in json as str
+            self.ui.extFeatCheckbox_hwClockUTCtime.setChecked(True)
+        else:
+            self.ui.extFeatCheckbox_hwClockUTCtime.setChecked(False)
+
+        self.ui.spinBox_processors.setValue(self.machineObject.getProcessors())
+        self.ui.spinBox_executionCap.setValue(self.machineObject.getExeCap())
+
+        if (self.machineObject.getExtFeat_enablePAE_NX() == "True"): # was in json as str
+            self.ui.checkBox_enablePAEnx.setChecked(True)
+        else:
+            self.ui.checkBox_enablePAEnx.setChecked(False)
+
+        self.ui.comboBox_paravirtInterface.setCurrentText(self.machineObject.getParavirtInterface())
+
+        if (self.machineObject.getHdVirt_enableVTx() == "True"): # was in json as str
+            self.ui.checkBox_enableVTxAMDV.setChecked(True)
+        else:
+            self.ui.checkBox_enableVTxAMDV.setChecked(False)
+
+        if (self.machineObject.getHdVirt_enableNestedPaging() == "True"): # was in json as str
+            self.ui.checkBox_enableNestedPaging.setChecked(True)
+        else:
+            self.ui.checkBox_enableNestedPaging.setChecked(False)
+
+    # Save the state of machine settings back into machine object when ok is pressed
+    def okPressed(self):       
+        self.machineObject.setBaseMemory(self.ui.spinbox_baseMemory.value())
+
+        if (self.ui.bootOrderCheckbox_hardDisk.isChecked() == True): # have to store back as string
+            self.machineObject.setBootOrder_hardDisk("True")
+        else:
+            self.machineObject.setBootOrder_hardDisk("False")
+
+        if (self.ui.bootOrderCheckbox_optical.isChecked() == True): # have to store back as string
+            self.machineObject.setBootOrder_optical("True")
+        else:
+            self.machineObject.setBootOrder_optical("False")
+
+        if (self.ui.bootOrderCheckBox_floppy.isChecked() == True): # have to store back as string
+            self.machineObject.setBootOrder_floppy("True")
+        else:
+            self.machineObject.setBootOrder_floppy("False")
+
+        if (self.ui.bootOrderCheckbox_network.isChecked() == True): # have to store back as string
+            self.machineObject.setBootOrder_network("True")
+        else:
+            self.machineObject.setBootOrder_network("False")
+
+        self.machineObject.setChipset(self.ui.comboBox_chipset.currentText())
+        self.machineObject.setPointingDevice(self.ui.comboBox_pointingDevice.currentText())
+
+        if (self.ui.extFeatCheckbox_enableIO.isChecked() == True): # have to store back as string
+            self.machineObject.setExtFeat_enableIO("True")
+        else:
+            self.machineObject.setExtFeat_enableIO("False")
+
+        if (self.ui.extFeatCheckBox_enableEFI.isChecked() == True): # have to store back as string
+            self.machineObject.setExtFeat_enableEFI("True")
+        else:
+            self.machineObject.setExtFeat_enableEFI("False")
+
+        if (self.ui.extFeatCheckbox_hwClockUTCtime.isChecked() == True): # have to store back as string
+            self.machineObject.setExtFeat_hwClockUTCtime("True")
+        else:
+            self.machineObject.setExtFeat_hwClockUTCtime("False")
+
+        self.machineObject.setProcessors(self.ui.spinBox_processors.value())
+        self.machineObject.setExeCap(self.ui.spinBox_executionCap.value())
+
+        if (self.ui.checkBox_enablePAEnx.isChecked() == True): # have to store back as string
+            self.machineObject.setExtFeat_enablePAE_NX("True")
+        else:
+            self.machineObject.setExtFeat_enablePAE_NX("False")
+
+        self.machineObject.setParavirtInterface(self.ui.comboBox_paravirtInterface.currentText())
+
+        if (self.ui.checkBox_enableVTxAMDV.isChecked() == True): # have to store back as string
+            self.machineObject.setHdVirt_enableVTx("True")
+        else:
+            self.machineObject.setHdVirt_enableVTx("False")
+
+        if (self.ui.checkBox_enableNestedPaging.isChecked() == True): # have to store back as string
+            self.machineObject.setHdVirt_enableNestedPaging("True")
+        else:
+            self.machineObject.setHdVirt_enableNestedPaging("False")
+
+        print("OK pressed, exiting \"Machine Settings\" window.")
+        
+
+    # if cancel is pressed, do not change the machine state
+    def cancelPressed(self):
+        print("Cancel pressed, exiting \"Machine Settings\" window.")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    test = '''{
+            "os": "windows",
+            "name": "attacker1",
+            "id": 122,
+            "type": "pov",
+            "shared_folders": [],
+            "network_settings": [],
+            "provisions": [],
+            "baseMemory": 5,
+            "bootOrder_floppy": "False",
+            "bootOrder_optical": "False",
+            "bootOrder_hardDisk": "False",
+            "bootOrder_network": "False",
+            "chipset": "PIIX3",
+            "pointingDevice": "PS/2 Mouse",
+            "extFeat_enableIO": "False",
+            "extFeat_enableEFI": "False",
+            "extFeat_hwClockUTCtime": "False",
+            "processors": 3,
+            "exeCap": 99,
+            "extFeat_EnablePAE_NX": "False",
+            "paravirtInterface": "Default",
+            "hdVirt_enableVTx": "False",
+            "hdVirt_enableNestedPaging": "False"
+        }'''
+    jsonObject = json.loads(test)
+    testMachine = BHMachine()
+    testMachine.fromJSON(jsonObject)
+    window = MachineSettings(testMachine)
+    window.show()
+
+    sys.exit(app.exec_())
